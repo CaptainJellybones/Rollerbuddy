81ac276ae1d39c78c3c24ab5bd8ddc82c9c4970c
CaptainJellybones <44872222+CaptainJellybones@users.noreply.github.com>
2020-09-01 23:25:11 +0200

    Building skeleton

--- js.js
+++ js.js
@@ -1,1 +1,21 @@
 console.log("Hello World");
+
+function calculate() {
+    console.log("Calculate Begin");
+    var netpower = document.getElementById("network-power").value;
+    console.log("Netpower = " + netpower);
+    switch (document.querySelector("#network-power").value) {
+        case "GH/s":
+            netpower * 1000000000;
+            console.log("Netpower after conversion: " + netpower);
+            break;
+        case "TH/s":
+            netpower * 1000000000000;
+            console.log("Netpower after conversion: " + netpower);
+            break;
+        default:
+            netpower * 1000000000000000;
+            console.log("Netpower after conversion: " + netpower);
+            break;
+    }
+}
