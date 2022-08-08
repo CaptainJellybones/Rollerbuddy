3add3dd0d48026d3a7422d3860e2a10eeb21eb14
CaptainJellybones <44872222+CaptainJellybones@users.noreply.github.com>
2022-08-08 14:37:13 +0200

    eventListener added for CSS toggle

--- js/js.js
+++ js/js.js
@@ -1,174 +1,29 @@
-console.log("Hello World");
+const rewardCards = document.querySelectorAll(".rewardBody");
+const coinCards = document.querySelectorAll('.coinCard');
 
-function blockAmount() {
-    // Returns pre-set block rewards based on block selected
-    // Update if updated on rollercoin
-    switch (document.getElementById("block-type").selectedIndex) {
-        case 0:
-            //bitcoin
-            var blockreward = 30000;
-            document.getElementById("block-reward").value = 30000;
-            break;
-        case 1:
-            //Doge
-            var blockreward = 20;
-            document.getElementById("block-reward").value = 20;
-            break;
-        case 2:
-            //eth
-            var blockreward = 0.005;
-            document.getElementById("block-reward").value = 0.005;
-            break;
-        case 3:
-            //bnb
-            var blockreward = 0.012;
-            document.getElementById("block-reward").value = 0.012;
-            break;
-        case 4: 
-            //matic
-            var blockreward = 3;
-            document.getElementById("block-reward").value = 3;
-            console.log("matic end");
-            break;
-        default:
-            //RLT
-            console.log("rlt start");
-            var blockreward = 30;
-            document.getElementById("block-reward").value = 30;
-            break;
-    }
-}
+// function visibilityToggle(element) {
+//     if (element.className == "hidden") {
+//         element.classList.remove('hidden');
+//         console.log("I've been clicked");
+//     } else {
+//         element.classList.add('hidden');
+//         console.log("I've been clicked");
+//     }
+// }
 
-function showHashpowerHelp() {
-    var x = document.querySelector("body > div.row.no-gutters.hashinput > div > div:nth-child(1) > img");
-    if (x.classList.contains("hide")) {
-        x.classList.remove("hide");
+function rewardViewToggle() {
+    if (element.classList.includes('hidden')) {
+        element.classList.remove('hidden');
     } else {
-        x.classList.add("hide");
+        element.classList.add('hidden');
     }
 }
 
-function supportMeToggle() {
-    var x = document.querySelector("body > div.supportMe");
-    if (x.classList.contains("hidden")) {
-        x.classList.add("animate__animate", "animate__fadeInLeft");
-        x.classList.remove("hidden");
-        console.log("revealed SupportMe")
-        // x.classList.add("animate__animated", "animate__bounceOutLeft"); 
-    } else {
-        x.classList.add("hidden");
-        console.log("Hide SupportMe")
-    }
+for (let i = 0; i < coinCards.length; i++) {
+    coinCards[i].addEventListener('click', rewardViewToggle);
+    console.log("eventlistener " + i + " added");
 }
 
-function calculateGoalPower() {
-    console.log("Calculate Begin");
-    var netpower = parseFloat(document.getElementById("network-power").value);
-    var goalpower = parseFloat(document.getElementById("goal-power").value);
-    var blockreward = parseFloat(document.getElementById("block-reward").value);
-
-    switch (document.getElementById("network-power-selector").selectedIndex) {
-        case 0:
-            netpower *= 1000000000;
-            console.log("Netpower after ghs conversion: " + netpower);
-            break;
-        case 1:
-            netpower *= 1000000000000;
-            console.log("Netpower after ths conversion: " + netpower);
-            break;
-        case 2:
-            netpower *= 1000000000000000;
-            console.log("Netpower after phs conversion: " + netpower);
-            break;
-        default:
-            netpower *= 1000000000000000000;
-            console.log("Netpower after ehs conversion: " + netpower);
-            break;
-    }
-    switch (document.getElementById("goal-power-selector").selectedIndex) {
-        case 0:
-            //GH/s
-            goalpower *= 1000000000;
-            console.log("goalpower after ghs conversion: " + goalpower);
-            break;
-        case 1:
-            //TH/s
-            goalpower *= 1000000000000;
-            console.log("goalpower after ths conversion: " + goalpower);
-            break;
-        case 2:
-            //PH/s
-            goalpower *= 1000000000000000;
-            console.log("goalpwer after phs conversion: " + goalpower);
-            break;
-        default:
-            //EH/s
-            goalpower *= 1000000000000000000;
-            console.log("goalpwer after ehs conversion: " + goalpower);
-            break;
-    }
-    console.log("Block: " + blockreward);
-    var exp_reward = blockreward * (goalpower / netpower);
-
-    console.log("exp. reward " + exp_reward.toFixed(4));
-
-    //All timers are now the same 2021-04-19
-    const BlockTimer = 600;
-    const secFullDay = 86400;
-
-    const dailyBlocks = secFullDay / BlockTimer;
-    console.log(dailyBtcBlocks + ", " + dailyDogeBlocks + ", " + dailyEthBlocks)
-
-    console.log("DailyBTC " + dailyBtcBlocks);
-    console.log("DailyDOGE " + dailyDogeBlocks);
-    console.log("DailyETH " + dailyEthBlocks);
-    console.log(exp_reward);
-
-    switch (document.getElementById("block-type").selectedIndex) {
-        case 0:
-            document.getElementById("exp_reward").innerHTML = exp_reward.toFixed(4) + " Satoshi";
-            var btcResult = (exp_reward * dailyBlocks).toFixed(4);
-            document.getElementById("daily").innerHTML = btcResult + " Satoshi";
-            document.getElementById("weekly").innerHTML = (btcResult * 7).toFixed(4) + " Satoshi";
-            document.getElementById("monthly").innerHTML = (btcResult * 30).toFixed(4) + " Satoshi";
-            break;
-        case 1:
-            document.getElementById("exp_reward").innerHTML = exp_reward.toFixed(4) + " DOGE";
-            var dogeResult = (exp_reward * dailyBlocks).toFixed(6);
-            document.getElementById("daily").innerHTML = dogeResult + " DOGE";
-            document.getElementById("weekly").innerHTML = (dogeResult * 7).toFixed(4) + " DOGE";
-            document.getElementById("monthly").innerHTML = (dogeResult * 30).toFixed(4) + " DOGE";
-            break;
-        case 2:
-            document.getElementById("exp_reward").innerHTML = exp_reward.toFixed(8) + " Ethereum";
-            var ethResult = (exp_reward * dailyBlocks).toFixed(8);
-            document.getElementById("daily").innerHTML = ethResult + " Ethereum";
-            document.getElementById("weekly").innerHTML = (ethResult * 7).toFixed(8) + " Ethereum";
-            document.getElementById("monthly").innerHTML = (ethResult * 30).toFixed(8) + " Ethereum";
-            break;
-        case 3:
-            document.getElementById("exp_reward").innerHTML = exp_reward.toFixed(8) + " BNB";
-            var bnbResult = (exp_reward * dailyBlocks).toFixed(8);
-            document.getElementById("daily").innerHTML = bnbResult + " BNB";
-            document.getElementById("weekly").innerHTML = (bnbResult * 7).toFixed(8) + " BNB";
-            document.getElementById("monthly").innerHTML = (bnbResult * 30).toFixed(8) + " BNB";
-            break;
-        case 4:
-            document.getElementById("exp_reward").innerHTML = exp_reward.toFixed(8) + " MATIC";
-            var maticResult = (exp_reward * dailyBlocks).toFixed(8);
-            document.getElementById("daily").innerHTML = maticResult + " MATIC";
-            document.getElementById("weekly").innerHTML = (maticResult * 7).toFixed(8) + " MATIC";
-            document.getElementById("monthly").innerHTML = (maticResult * 30).toFixed(8) + " MATIC";
-            break;
-        default:
-            document.getElementById("exp_reward").innerHTML = exp_reward.toFixed(8) + " Rollertoken";
-            var rltResult = (exp_reward * dailyBlocks).toFixed(4);
-            document.getElementById("daily").innerHTML = rltResult + " Rollertoken";
-            document.getElementById("weekly").innerHTML = (rltResult * 7).toFixed(4) + " Rollertoken";
-            document.getElementById("monthly").innerHTML = (rltResult * 30).toFixed(4) + " Rollertoken";
-            break;
-    }
-}
-
-// Arc.io Div appender
-
+// coinCards.forEach(card => {
+//     card.addEventListener('click', rewardViewToggle(card))
+// })
