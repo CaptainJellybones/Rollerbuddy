27f6d7db891933b153f35e56833dec379ba8d75f
CaptainJellybones <44872222+CaptainJellybones@users.noreply.github.com>
2022-08-09 01:17:19 +0200

    It aint pretty, but it works

--- js/js.js
+++ js/js.js
@@ -1,6 +1,31 @@
 const rewardCards = document.querySelectorAll(".rewardBody");
 const coinCards = document.querySelectorAll('.coinCard');
 
+let testCard = document.querySelector("body > div.container.coins > div.row.firstPowerRow > div:nth-child(4) > div");
+
+// May god forgive me for this function
+function rewardViewToggle(element) {
+    element.currentTarget.children[1].children[2].children[1].classList.toggle('hidden');
+    element.currentTarget.children[1].children[3].classList.toggle('imgFlip');
+}
+
+for (let i = 0; i < coinCards.length; i++) {
+    coinCards[i].addEventListener('click', rewardViewToggle);
+    console.log("eventlistener " + i + " added");
+}
+
+// function expand(event) {
+//     if (event.current.classList.includes('hidden')) {
+//         alert("You clicked while hidden");
+//     } else {
+//         alert("You clicked while visible");
+//     }
+// }
+
+// coinCards.forEach(card => {
+//     card.addEventListener('click', rewardViewToggle(card))
+// })
+
 // function visibilityToggle(element) {
 //     if (element.className == "hidden") {
 //         element.classList.remove('hidden');
@@ -10,20 +35,3 @@ const coinCards = document.querySelectorAll('.coinCard');
 //         console.log("I've been clicked");
 //     }
 // }
-
-function rewardViewToggle() {
-    if (element.classList.includes('hidden')) {
-        element.classList.remove('hidden');
-    } else {
-        element.classList.add('hidden');
-    }
-}
-
-for (let i = 0; i < coinCards.length; i++) {
-    coinCards[i].addEventListener('click', rewardViewToggle);
-    console.log("eventlistener " + i + " added");
-}
-
-// coinCards.forEach(card => {
-//     card.addEventListener('click', rewardViewToggle(card))
-// })
