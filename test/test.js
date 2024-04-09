136aab5a01ffac0ae164b47cc826f70c26d49623
CaptainJellybones <44872222+CaptainJellybones@users.noreply.github.com>
2024-04-09 10:24:27 +0200

    reinstall commit

--- test/test.js
+++ test/test.js
@@ -3,7 +3,12 @@ function openNav() {
     document.getElementById("buddySidenav").style.width = "250px";
   }
   
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("buddySidenav").style.width = "0";
-  }
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("buddySidenav").style.width = "0";
+}
+
+function getNetPower() {
+  netpower = $("#iFrame").contents().find(".currency-info-power").innerText;
+  netpowerJS = iframe.contentWindow.document;
+}
